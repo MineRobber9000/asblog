45837bfe9e7dda8d063cffc172c07e4608806c41
khuxkm fbexl <khuxkm@ttm.sh>
2019-04-07 12:57:45 -0400

    Change excerpt logic and add publish shorthand in Makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
+PUBLISH_DIR:=/home/khuxkm/public_html/blog
+
 blog: in index.md
 	autosite --auto
 
-.PHONY: in
+.PHONY: in publish
 in: $(wildcard posts/*.md)
 	python add_meta.py
 
 index.md: $(wildcard posts/*.md)
 	python generate_index.py
+
+publish: blog
+	rm -rf $(PUBLISH_DIR)
+	mv out $(PUBLISH_DIR)
--- add_meta.py
+++ add_meta.py
@@ -11,8 +11,8 @@ for post in POSTS:
 	if not postc.get("published",False): continue
 	title = postc.get("title")
 	pslug = slug(title)
-	excerpt = truncate(re.split("\n{2,}",postc.content)[0],postc.get("excerpt_length"))
+	excerpt = truncate(re.sub(r"\[([^\]]+)\]\([^)]+\)",r"\g<1>",re.split("\n{2,}",postc.content)[0].replace("\n"," ").replace("  "," ")),postc.get("excerpt_length"))
 	with open("in/{}.md".format(pslug),"w") as f:
 		f.write("<!-- attrib title: {} -->\n<!-- attrib description: {}-->\n<!-- attrib template: post -->\n<!-- attrib sitename: {} -->\n\n".format(title,excerpt,BLOG_NAME))
-		f.write("# {}\n## published {}\n\n".format(title,postc.get("pubdate","1970-01-01")))
+		f.write("# {} (published {})\n\n".format(title,postc.get("pubdate","1970-01-01")))
 		f.write(postc.content)
