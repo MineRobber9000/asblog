972826fe2f2e0715c48aab3d603f9a6e20a39a02
khuxkm fbexl <khuxkm@ttm.sh>
2020-03-30 04:56:04 -0400

    Add Atom feed support
    Also, make generate_index behave like add_meta.

--- config.py
+++ config.py
@@ -16,12 +16,17 @@ POSTS_SHOWN = 3
 BLOG_NAME = "AutoSite-Blog"
 BLOG_DESCRIPTION = "This is a set of scripts that makes AutoSite by dotcomboom work as a Jekyll alternative."
 
+# URL where blog will be made public, without the trailing slash.
+BLOG_URL = "https://example.com/blog"
+
 # used to prevent duplication of effort
 
-import frontmatter,os
+import frontmatter,os,os.path,datetime,tzlocal
 def load(fn):
 	with open(fn) as f:
 		return frontmatter.load(f)
 POSTS = os.listdir(POSTS_DIR)
 POSTS = [os.path.join(POSTS_DIR,x) for x in POSTS]
 POSTS = {post: load(post) for post in POSTS}
+for fn, post in POSTS.values():
+	post["mtime"]=datetime.datetime.fromtimestamp(os.path.getmtime(fn),tzlocal.get_localzone()).isoformat()
--- generate_index.py
+++ generate_index.py
@@ -1,4 +1,4 @@
-import os,re,frontmatter,datetime
+import os,re,frontmatter,datetime,uuid
 from config import *
 from util import *
 import markdown
@@ -13,13 +13,11 @@ for post in POSTS:
 	postc = POSTS[post]
 	if not postc.get("published",False): continue
 	title = postc.get("title")
-	pslug = slug(title)
-	first_paragraph = re.split("\n{2,}",postc.content)[0].replace("\n"," ").replace("  "," ")
-	# convert to real text
-	first_paragraph = soup(gfm.convert(first_paragraph),"html.parser").text
+	pslug = slug(postc)
+	first_paragraph = soup(gfm.convert(postc.content),"html.parser").find("p").text.replace("\n"," ").replace("  "," ")
 	excerpt = truncate(first_paragraph,postc.get("excerpt_length"))
 	pubdate = postc.get("pubdate")
-	ENTRIES.append(dict(title=title,pslug=pslug,pubdate=pubdate,excerpt=excerpt))
+	ENTRIES.append(dict(title=title,pslug=pslug,pubdate=pubdate,excerpt=excerpt,mtime=postc.get("mtime")))
 
 ENTRIES.sort(key=lambda x: x["pubdate"])
 ENTRIES.reverse()
@@ -31,3 +29,33 @@ with open("in/index.md","w") as f:
 	f.write("# {}\n\n".format(BLOG_NAME))
 	for post in ENTRIES:
 		f.write("## [{post[title]}](./{post[pslug]}.html) - published {post[pubdate]}\n\n{post[excerpt]}\n\n".format(post=post))
+
+UUID = None
+if not os.path.exists(".uuid"):
+	UUID = uuid.uuid4().urn
+	with open(".uuid","w") as f:
+		f.write(UUID)
+else:
+	with open(".uuid") as f:
+		UUID = f.read().strip()
+with open("includes/feed.atom","w") as f:
+	f.write("<?xml version=1.0 encoding=\"utf-8\"?>\n\n")
+	f.write("<feed xmlns=\"http://www.w3.org/2005/Atom\">\n")
+	f.write("<title>"+BLOG_NAME+"</title>\n<subtitle>"+BLOG_DESCRIPTION+"</subtitle>\n")
+	f.write("<link rel=\"self\" href=\""+BLOG_URL+"/feed.atom\" />\n<link href=\""+BLOG_URL+"\" />\n")
+	f.write("<id>"+UUID+"</id>\n")
+	f.write("<author><name>"+BLOG_NAME+"</name></author>\n")
+	for post in ENTRIES:
+		f.write("<entry>\n")
+		# title
+		f.write("\t<title>"+post["title"]+"</title>\n")
+		# link
+		f.write("\t<link rel=\"alternate\" type=\"text/html\" href=\""+BLOG_URL+"/"+post["pslug"]+".html\" />\n")
+		# updated
+		f.write("\t<updated>"+post["mtime"]+"</updated>\n")
+		# summary
+		f.write("\t<summary>"+post["excerpt"]+"</summary>\n")
+		# id
+		f.write("\t<id>"+BLOG_URL+"/"+post["pslug"]+".html</id>\n")
+		f.write("</entry>\n")
+	f.write("</feed>\n")
